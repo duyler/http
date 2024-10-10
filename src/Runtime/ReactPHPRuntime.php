328946c7e2eb174a831c443fc143b232ace5cbfd
Mikhail Ilinsky <milinsky@MacBook-Pro--Milinsky.local>
2024-10-10 17:03:14 +1000

    Sync with Builder

--- src/Runtime/ReactPHPRuntime.php
+++ src/Runtime/ReactPHPRuntime.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Duyler\Http\Runtime;
 
-use Duyler\Builder\Builder;
+use Duyler\Builder\ApplicationBuilder;
 use Duyler\DI\ContainerInterface;
 use Duyler\EventBus\BusInterface;
 use Duyler\EventBus\Dto\Event;
@@ -25,13 +25,15 @@ final class ReactPHPRuntime implements RuntimeInterface
 
     public function __construct()
     {
-        $builder = new Builder();
-        $builder->loadPackages();
-        $builder->loadBuild();
+        $builder = new ApplicationBuilder();
+        $this->bus = $builder
+            ->getBusBuilder()
+            ->loadPackages()
+            ->loadBuild()
+            ->build();
 
         $this->container = $builder->getContainer();
         $this->errorHandler = $this->container->get(ErrorHandler::class);
-        $this->bus = $builder->build();
     }
 
     public function run(): void
@@ -52,20 +54,24 @@ final class ReactPHPRuntime implements RuntimeInterface
 
                 /** @var ResponseInterface $response */
                 $response = $this->bus->getResult('Http.PersistResponse')->data;
-                $this->bus->reset();
-                $this->container->finalize();
 
                 return $response;
             } catch (Throwable $e) {
-                $this->bus->reset();
-                $this->container->finalize();
-                $this->errorHandler->handle($e);
-                echo (string) $e;
-                throw $e;
+                return $this->errorHandler->handle($e);
+            } finally {
+                $this->finalize();
             }
         });
 
         $socket = new SocketServer('0.0.0.0:80');
         $http->listen($socket);
+    }
+
+    private function finalize(): void
+    {
+        $this->bus->reset();
+        $this->container->finalize();
+
+        gc_collect_cycles();
     }
 }
